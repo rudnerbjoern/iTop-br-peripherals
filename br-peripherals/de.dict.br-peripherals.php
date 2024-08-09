51d4c37c04e12f4b1974339e8b8ca029b1517c26
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-08-09 10:31:01 +0200

    initial commit

---
+++ br-peripherals/de.dict.br-peripherals.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-08-09
+ *
+ * Localized data
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    'Class:Monitor' => 'Bildschirm',
+    'Class:Monitor+' => 'Ein Bildschirm',
+    'Class:Keyboard' => 'Tastatur',
+    'Class:Keyboard+' => 'Eine Tastatur',
+    'Class:Mouse' => 'Maus',
+    'Class:Mouse+' => 'Eine Computermaus',
+    'Class:Headset' => 'Headset',
+    'Class:Headset+' => 'Ein Headset',
+    // Class:Model
+    'Class:Model/Attribute:type/Value:Monitor' => 'Bildschirm',
+    'Class:Model/Attribute:type/Value:Monitor+' => 'Bildschirm',
+    'Class:Model/Attribute:type/Value:Keyboard' => 'Tastatur',
+    'Class:Model/Attribute:type/Value:Keyboard+' => 'Tastatur',
+    'Class:Model/Attribute:type/Value:Mouse' => 'Maus',
+    'Class:Model/Attribute:type/Value:Mouse+' => 'Maus',
+    'Class:Model/Attribute:type/Value:Headset' => 'Headset',
+    'Class:Model/Attribute:type/Value:Headset+' => 'Headset',
+));
---
+++ br-peripherals/module.br-peripherals.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-008-09
+ *
+ * iTop module definition file
+ */
+
+SetupWebPage::AddModule(
+    __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+    'br-peripherals/0.0.1',
+    array(
+        // Identification
+        //
+        'label' => 'Datamodel: Peripheral devices for PCs',
+        'category' => 'business',
+
+        // Setup
+        //
+        'dependencies' => array(
+            '(itop-config-mgmt/2.5.0 & itop-config-mgmt/<3.0.0)||itop-structure/3.0.0',
+            'itop-endusers-devices/2.7.5',
+        ),
+        'mandatory' => false,
+        'visible' => true,
+
+        // Components
+        //
+        'datamodel' => array(
+            'model.br-peripherals.php'
+        ),
+        'webservice' => array(),
+        'data.struct' => array(),
+        'data.sample' => array(),
+
+        // Documentation
+        //
+        'doc.manual_setup' => '',
+        'doc.more_information' => '',
+
+        // Default settings
+        //
+        'settings' => array(),
+    )
+);
