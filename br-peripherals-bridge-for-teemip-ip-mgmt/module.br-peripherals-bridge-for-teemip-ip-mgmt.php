8dcaa6c226f4199776aecc284ae4dcd1db5e7cca
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-08-16 13:24:02 +0200

    add bridge to TeemIp IP-Management

--- br-peripherals-bridge-for-teemip-ip-mgmt/module.br-peripherals-bridge-for-teemip-ip-mgmt.php
+++ br-peripherals-bridge-for-teemip-ip-mgmt/module.br-peripherals-bridge-for-teemip-ip-mgmt.php
@@ -9,29 +9,31 @@
  */
 
 SetupWebPage::AddModule(
-    __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-peripherals/0.3.0',
+    __FILE__,
+    'br-peripherals-bridge-for-teemip-ip-mgmt/0.1.0',
     array(
         // Identification
         //
-        'label' => 'Datamodel: Peripheral devices for PCs',
+        'label' => 'Bridge - BR-Peripherals + TeemIp IP Management',
         'category' => 'business',
 
         // Setup
         //
         'dependencies' => array(
-            '(itop-config-mgmt/2.5.0 & itop-config-mgmt/<3.0.0)||itop-structure/3.0.0',
-            'itop-endusers-devices/2.7.5',
+            'itop-endusers-devices/2.7.0',
+            'teemip-ip-mgmt/3.0.1',
+            'teemip-config-mgmt-adaptor/3.0.1',
+            'br-peripherals/0.3.0',
         ),
         'mandatory' => false,
-        'visible' => true,
+        'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
+        'auto_select' => 'SetupInfo::ModuleIsSelected("itop-endusers-devices") && SetupInfo::ModuleIsSelected("teemip-ip-mgmt") && SetupInfo::ModuleIsSelected("teemip-config-mgmt-adaptor") && SetupInfo::ModuleIsSelected("br-peripherals")',
 
         // Components
         //
         'datamodel' => array(
-            'model.br-peripherals.php'
+            'model.br-peripherals-bridge-for-teemip-ip-mgmt.php'
         ),
-        'webservice' => array(),
         'data.struct' => array(),
         'data.sample' => array(),
 
