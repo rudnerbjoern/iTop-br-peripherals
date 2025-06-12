267c710effdc3df6b7d770b350e8e76a50e2db59
Bjoern Rudner <rudnerbjoern@hotmail.com>
2025-06-12 13:20:17 +0200

    set compatibility tp iTop 3.2

--- br-peripherals-bridge-for-teemip-ip-mgmt/module.br-peripherals-bridge-for-teemip-ip-mgmt.php
+++ br-peripherals-bridge-for-teemip-ip-mgmt/module.br-peripherals-bridge-for-teemip-ip-mgmt.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (C) 2024-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2025-06-11
+ * @version     2025-06-12
  *
  * iTop module definition file
  */
@@ -11,7 +11,7 @@
 /** @disregard P1009 Undefined type SetupWebPage */
 SetupWebPage::AddModule(
     __FILE__,
-    'br-peripherals-bridge-for-teemip-ip-mgmt/3.1.12',
+    'br-peripherals-bridge-for-teemip-ip-mgmt/3.2.0',
     array(
         // Identification
         //
@@ -21,9 +21,9 @@ SetupWebPage::AddModule(
         // Setup
         //
         'dependencies' => array(
-            'br-peripherals/3.1.12||teemip-ip-mgmt/3.0.0',
-            'br-peripherals/3.1.12||teemip-config-mgmt-adaptor/3.0.0',
-            'br-peripherals/3.1.12',
+            'br-peripherals/3.2.0||teemip-ip-mgmt/3.0.0',
+            'br-peripherals/3.2.0||teemip-config-mgmt-adaptor/3.0.0',
+            'br-peripherals/3.2.0',
         ),
         'mandatory' => false,
         'visible' => true, // To prevent auto-install but shall not be listed in the install wizard
--- br-peripherals/dictionaries/de.dict.br-peripherals.php
+++ br-peripherals/dictionaries/de.dict.br-peripherals.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * @copyright   Copyright (C) 2024 Björn Rudner
+ * @copyright   Copyright (C) 2024-2025 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-10-18
+ * @version     2025-06-12
  *
  * Localized data
  */
