4a1850691aebd5387d149863b6287fc6d2a67b7d
Björn Rudner <rudnerbjoern@hotmail.com>
2026-08-28 16:49:32 +0200

    add Russian localization for peripherals and update English dictionary entries

---
+++ br-peripherals-bridge-for-teemip-ip-mgmt/dictionaries/ru.dict.br-peripherals-bridge-for-teemip-ip-mgmt.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024-2025 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2025-06-12
+ *
+ * Localized data
+ */
+
+/** @disregard P1009 Undefined type Dict */
+Dict::Add('RU RU', 'Russian', 'Русский', array(
+    'Class:DockingStation/Attribute:macaddress' => 'MAC-адрес',
+    'Class:DockingStation/Attribute:macaddress+' => '',
+    'Class:DockingStation/Attribute:ipaddress_id' => 'IP-адрес',
+    'Class:DockingStation/Attribute:ipaddress_id+' => '',
+    'Class:DockingStation/Attribute:ipaddress_name' => 'Имя IP-адреса',
+    'Class:DockingStation/Attribute:ipaddress_name+' => '',
+    'Class:ConferenceSystem/Attribute:macaddress' => 'MAC-адрес',
+    'Class:ConferenceSystem/Attribute:macaddress+' => '',
+    'Class:ConferenceSystem/Attribute:ipaddress_id' => 'IP-адрес',
+    'Class:ConferenceSystem/Attribute:ipaddress_id+' => '',
+    'Class:ConferenceSystem/Attribute:ipaddress_name' => 'Имя IP-адреса',
+    'Class:ConferenceSystem/Attribute:ipaddress_name+' => '',
+    'Class:Scanner/Attribute:macaddress' => 'MAC-адрес',
+    'Class:Scanner/Attribute:macaddress+' => '',
+    'Class:Scanner/Attribute:ipaddress_id' => 'IP-адрес',
+    'Class:Scanner/Attribute:ipaddress_id+' => '',
+    'Class:Scanner/Attribute:ipaddress_name' => 'Имя IP-адреса',
+    'Class:Scanner/Attribute:ipaddress_name+' => '',
+));
